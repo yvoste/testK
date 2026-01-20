3141146768eef2d1c10c40a84ec934c24e285270
yvoste <yves.mitaine@nordnet.fr>
2026-01-20 14:27:52 +0100

    conatct

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,21 +1,21 @@
 const Contact = () => {
-  return (
-    <div className="contact">
-      <h2>Contact Me</h2>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
+    return (
+        <div className="contact">
+            <h2>Prendre contact avec moi</h2>
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
 
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" required></textarea>
 
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
 };
 
 export default Contact;
